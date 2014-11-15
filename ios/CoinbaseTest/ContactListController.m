a564485f10cefb8649cf77636a17bb9ba92ad21e
Eli White <github@eli-white.com>
2014-11-15 01:59:10 -0800

    Adding a contact list controller. Not finished

---
+++ ios/CoinbaseTest/ContactListController.m
@@ -0,0 +1,22 @@
+//
+//  ContactListController.m
+//  CoinbaseTest
+//
+//  Created by Eli White on 11/15/14.
+//  Copyright (c) 2014 Conrad Kramer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+@import AddressBook;
+#import "ContactListController.h"
+
+@implementation ContactListController
+
+- (NSString *) GetContactList {
+    NSLog(@"Hello, World!");
+    
+    NSString *temp = @"Contrafibularity";
+    return temp;
+}
+
+@end
--- ios/CoinbaseTest/WebViewController.m
+++ ios/CoinbaseTest/WebViewController.m
@@ -8,6 +8,7 @@
 
 #import "WebViewController.h"
 #import "QRCodeViewController.h"
+#import "ContactListController.h"
 
 @interface WebViewController () <UIWebViewDelegate, QRCodeViewControllerDelegate>
 
@@ -49,6 +50,110 @@ static NSString * const CoinbaseBaseURLString = @"http://drawer.ngrok.com/";
     return UIStatusBarStyleLightContent;
 }
 
+
+//- (NSString *)readContactList {
+//    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+//    CFArrayRef people = ABAddressBookCopyArrayOfAllPeople(addressBook);
+//
+//    CFIndex numberOfPeople = ABAddressBookGetPersonCount(addressBook);
+//
+//    NSMutableDictionary *myDictionary = [[NSMutableDictionary alloc] init];
+//
+//    for(int i = 0; i < numberOfPeople; i++) {
+//
+//        ABRecordRef person = CFArrayGetValueAtIndex( people, i );
+//
+//        NSString *firstName = (__bridge NSString *)(ABRecordCopyValue(person, kABPersonFirstNameProperty));
+//        NSString *lastName = (__bridge NSString *)(ABRecordCopyValue(person, kABPersonLastNameProperty));
+//        NSString *name = [NSString stringWithFormat:@"%@ %@", firstName, lastName];
+//
+////        kABPersonCompositeNameFormatFirstNameFirst
+//
+//        ABMultiValueRef phoneNumbers = ABRecordCopyValue(person, kABPersonPhoneProperty);
+//
+//        if(ABMultiValueGetCount(phoneNumbers) >= 1) {
+//            NSString *phoneNumber = (__bridge_transfer NSString *) ABMultiValueCopyValueAtIndex(phoneNumbers, 0);
+//            [myDictionary setObject:name  forKey:phoneNumber];
+//        }
+//
+//        NSLog(@"=============================================");
+//
+//    }
+//    NSError *error = nil;
+//    NSData *json;
+//
+//    // Dictionary convertable to JSON ?
+//    if ([NSJSONSerialization isValidJSONObject:myDictionary])
+//    {
+//        // Serialize the dictionary
+//        json = [NSJSONSerialization dataWithJSONObject:myDictionary options:NSJSONWritingPrettyPrinted error:&error];
+//
+//        // If no errors, let's view the JSON
+//        if (json != nil && error == nil)
+//        {
+//            NSString *jsonString = [[NSString alloc] initWithData:json encoding:NSUTF8StringEncoding];
+//
+//            NSLog(@"JSON: %@", jsonString);
+//            return jsonString;
+//        }
+//    }
+//
+//    return NULL;
+//}
+//
+//- (NSString *)getContactList {
+//    CFErrorRef error = nil;
+//    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, &error);
+//    if (addressBook) {
+//        ABAddressBookRequestAccessWithCompletion(addressBook, ^(bool granted, CFErrorRef error) {
+//            if (granted) {
+//
+//                CFArrayRef people = ABAddressBookCopyArrayOfAllPeople(addressBook);
+//                for (CFIndex idx = 0; idx < CFArrayGetCount(people); idx++) {
+//                    ABRecordRef person = CFArrayGetValueAtIndex(people, idx);
+//                    NSString *name = (__bridge NSString *)ABRecordCopyCompositeName(person);
+//                    NSLog(@"SOMEONE NAMED %@", name);
+//                }
+//            } else {
+//                // Send error
+//            }
+//        });
+//    } else {
+//        // Send error
+//    }
+//
+//
+//    if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusDenied ||
+//        ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusRestricted){
+//        //1
+//        NSLog(@"Denied");
+//        return NULL;
+//    } else if (ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusAuthorized){
+//        //2
+//        NSLog(@"Authorized");
+//
+////        return [self readContactList];
+//    } else{ //ABAddressBookGetAuthorizationStatus() == kABAuthorizationStatusNotDetermined
+//        //3
+//        NSLog(@"Not determined");
+//
+//        ABAddressBookRequestAccessWithCompletion(ABAddressBookCreateWithOptions(NULL, nil), ^(bool granted, CFErrorRef error){
+//            if (!granted){
+//                //4
+//                NSLog(@"Just denied");
+//                return NULL;
+//            }
+//            //5
+//            NSLog(@"Just authorized");
+//
+////            return [self readContactList];
+//        });
+//    }
+//
+//    return NULL;
+//}
+
+
 #pragma mark - UIWebViewDelegate
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
@@ -60,12 +165,36 @@ static NSString * const CoinbaseBaseURLString = @"http://drawer.ngrok.com/";
             UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:codeViewController];
             [self presentViewController:navigationController animated:YES completion:nil];
         }
+        else if ([components.host isEqualToString:@"getContacts"]) {
+            ContactListController *contactListController = [[ContactListController alloc] init];
+            NSString *contacts = [contactListController GetContactList];
+            
+            NSString *javascript = [NSString stringWithFormat:@"contactResult('%@')", contacts];
+            [self.webView stringByEvaluatingJavaScriptFromString:javascript];
+        }
     }
     
     return YES;
 }
 
 #pragma mark - QRCodeViewControllerDelegate
+
+- (void)codeViewController:(QRCodeViewController *)codeViewController didScanString:(NSString *)string {
+    [codeViewController dismissViewControllerAnimated:YES completion:^{
+        NSString *javascript = [NSString stringWithFormat:@"qrCodeResult('%@')", string];
+        [self.webView stringByEvaluatingJavaScriptFromString:javascript];
+    }];
+}
+
+- (void)codeViewControllerDidCancel:(QRCodeViewController *)codeViewController {
+    [codeViewController dismissViewControllerAnimated:YES completion:^{
+        NSString *javascript = @"qrCodeResult(undefined)";
+        [self.webView stringByEvaluatingJavaScriptFromString:javascript];
+    }];
+}
+
+#pragma mark - ContactListControllerDelegate
+
 
 - (void)codeViewController:(QRCodeViewController *)codeViewController didScanString:(NSString *)string {
     [codeViewController dismissViewControllerAnimated:YES completion:^{
