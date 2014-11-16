2e63a473c2be2a264ced82bc258a0c8129c45543
Eli White <github@eli-white.com>
2014-11-15 16:22:14 -0800

    Adding autocomplete dropdown for contact list

--- client/public/javascript/sections/newTrans.js
+++ client/public/javascript/sections/newTrans.js
@@ -1,25 +1,24 @@
-define(["section", 'tapHandler', 'platform', 'event', 'completely'], function(Section, TapHandler, Platform, Event, Completely) {
+define(["section", 'tapHandler', 'platform', 'event', 'completely'], function (Section, TapHandler, Platform, Event, Completely) {
+
+  var newContacts;
+  var names = [];
 
   var NewTrans = Section.extend({
     id: "page-new-trans",
+    _auto: null,
+    _recipient: null,
 
-    init: function() {
+    init: function () {
       this._super();
+
+      this._recipient = document.getElementById("recipient");
+
+      this._recipient.addEventListener("keyup", this._nameChanged.bind(this));
     },
 
-    show:function() {
-    	var auto = Completely(document.getElementById('container'), {
-	      fontSize : '24px',
-	      fontFamily : 'Arial',
-	      color:'#933',
-	      });
-	    auto.options = ['cocoa','coffee','orange'];
-	    auto.repaint(); 
-	    setTimeout(function() {
-	    auto.input.focus();
-	    },0);
+    show: function () {
 
-      window.contactResult = (function(contacts) {
+      window.contactResult = (function (contacts) {
         window.contactResult = undefined;
         this._gotContacts(contacts);
       }).bind(this);
@@ -27,25 +26,39 @@ define(["section", 'tapHandler', 'platform', 'event', 'completely'], function(Se
       window.location = 'coinbase://getContacts';
     },
 
-    _gotContacts: function(contacts) {
-      var keys = Object.keys(contacts);
-
-      var newFormat = {};
-
-      keys.forEach(function(key) {
+    _gotContacts: function (contacts) {
+      for (var name in contacts) {
         var newKey = '';
 
-        for (var i = 0; i < key.length; i++) {
-          if (parseInt(key[i])) {
-            newKey += key[i];
+        for(var i = 0; i < contacts[name].length; i++) {
+          if(parseInt(contacts[name][i])) {
+            newKey += contacts[name][i];
           }
         }
 
-        newFormat[newKey] = contacts[key];
-      });
+        contacts[name] = newKey;
+      }
+      newContacts = contacts;
+      names = Object.keys(contacts);
 
-      console.log("Contacts", newFormat);
+      this._updateAutocomplete();
     },
+
+    _updateAutocomplete: function () {
+      var vals = names.filter((function (item) {
+          return item.toLowerCase().indexOf(this._recipient.value.toLowerCase()) === 0;
+        }).bind(this))
+        .sort()
+        .map(function (name) {
+          return '<option value="' + newContacts[name] + '">' + name + '</option>';
+        });
+
+      document.getElementById('dropdown').innerHTML = vals;
+    },
+
+    _nameChanged: function (e) {
+      this._updateAutocomplete();
+    }
   });
 
   return new NewTrans();
